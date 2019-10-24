c73cdd9c22d6cbf780161364f32435e20cc3f22e
Sergey111991KV <sergey.cosilov@gmail.com>
2019-10-24 12:07:22 +0300

    first

--- FuelConsumption/ContentView.swift
+++ FuelConsumption/ContentView.swift
@@ -9,8 +9,89 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var beginingKilometres = "0"
+    @State private var endKilometres = "0"
+    @State private var allKilometres = "0"
+    @State private var massa = "0"
+    @State private var beginingOil = "0"
+    @State private var endOil = "0"
+    @State private var time = "0"
+    
+    
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            
+            
+            
+        List{
+            Section(header: Text("Distanse")){
+            HStack{
+             
+           Image("distance")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100,height:100)
+                
+                VStack{
+            VStack{
+              Text("Begining Kilometres")
+                TextField("0", text: $beginingKilometres)
+                    .keyboardType(.decimalPad)
+                    
+                 
+            }
+                VStack{
+             
+                
+               Text("End Kilometres")
+                               
+                TextField("0", text: $beginingKilometres)
+                               .keyboardType(.decimalPad)
+            }
+                
+                Text("All kilometres \(allKilometres)")
+            }
+            }
+            }
+             Section(header: Text("111")){
+                            Image("distance")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100,height:100)
+                Text("Begining Oil")
+                           
+                         TextField("0", text: $beginingKilometres)
+                           .keyboardType(.decimalPad)
+                           
+                           Text("End Oil")
+                                          
+                           TextField("0", text: $beginingKilometres)
+                                          .keyboardType(.decimalPad)
+                       }
+            
+             Section(header: Text("111")){
+                           
+                         Text("Begining Kilometres")
+                           
+                         TextField("0", text: $beginingKilometres)
+                           .keyboardType(.decimalPad)
+                           
+                           Text("End Kilometres")
+                                          
+                           TextField("0", text: $beginingKilometres)
+                                          .keyboardType(.decimalPad)
+                       }
+            Section{
+                Button("Show Alert") {
+                    
+                }
+            }
+            
+        }
+        .navigationBarTitle("Fuel Consumption")
+        }
     }
 }
 
